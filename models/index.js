ee2fe68adf0b5298a4c718e21a301b7581a5db36
Alissa Cohan <alissacohan18@gmail.com>
2023-02-05 12:01:05 -0500

    define model associations; create API routes for Post model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,15 @@
 // Import the User & Post model
-const User = require('./User');
+const User = require("./User");
 const Post = require("./Post");
+
+// create associations
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
 // Export an object with them as the properties
 module.exports = { User, Post };
