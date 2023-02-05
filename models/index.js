c6b5f8a5e0f3291e77e7764af02fe6db84b39b74
Alissa Cohan <alissacohan18@gmail.com>
2023-02-05 16:08:22 -0500

    include Votes in get routes

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 // Import the User & Post model
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require("./Vote");
 
 // create associations
+// one-to-many
 User.hasMany(Post, {
   foreignKey: "user_id",
 });
@@ -11,5 +13,35 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
+// many-to-many
+User.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
+});
+
+//connect Post & Vote, and User & Vote
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
 // Export an object with them as the properties
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
