12c193bb644aee45b3f5ff9bbfbc62265747902f
Alissa Cohan <alissacohan18@gmail.com>
2023-02-07 19:34:53 -0500

    implement use of handlebars and updated structure to follow MVC

---
+++ controllers/home-routes.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+router.get("/", (req, res) => {
+  console.log("======================");
+  Post.findAll({
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+          ),
+          "vote_count",
+        ],
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    })
+    .then((dbPostData) => {
+//loop over & map each Sequelize obj into a serialized version of itself & saving results in a new posts array
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      
+      //use .render vs .sendFile() for using template engine->specify which template to use
+      //the .handlebars extension is implied
+      res.render("homepage", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
+  
+  module.exports = router;
+  
